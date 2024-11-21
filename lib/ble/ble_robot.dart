cb8a0d45c051816d214a2f5dc22f8574ea02ca13
Marcel Landry <marcel.landry@cegep-rimouski.qc.ca>
2024-11-21 12:01:38 -0500

    Modification des packages.

--- lib/ble/ble_robot.dart
+++ lib/ble/ble_robot.dart
@@ -1,98 +1,117 @@
 // ignore_for_file: constant_identifier_names
 import 'package:flutter/material.dart';
-import 'ble_manager.dart';
-import 'ble_peripheral.dart';
+import 'dart:convert';
+//import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:flutter_blue_plus_windows/flutter_blue_plus_windows.dart';
 
 ///Liste des actions possibles pour le robot.
-enum RobotActionType
-{
-    Forward,
-    Back,
-    TurnLeft,
-    TurnRight,
-    Stop,
-    MoveSequenceOne,
-    MoveSequenceTwo,
-    AvoidObstacleOne,
-    AvoidObstacleTwo,
+enum RobotActionType {
+  Forward,
+  Back,
+  TurnLeft,
+  TurnRight,
+  Stop,
+  MoveSequenceOne,
+  MoveSequenceTwo,
+  AvoidObstacleOne,
+  AvoidObstacleTwo,
 }
-
 
 ///Liste des types de télémétrie disponibles.
-enum TelemetryType
-{
-    Distance,
-    LastActions,
-    LastError,
+enum TelemetryType {
+  Distance,
+  LastActions,
+  LastError,
 }
-
 
 /// Classe BLERobot qui gère la communication Bluetooth Low Energy (BLE) avec le robot.
 class BLERobot {
-  late BLEPeripheral _peripheral;
-  late BLEManager _manager;
+  late BluetoothDevice? connectedDevice;
+  late BluetoothCharacteristic? commandCharacteristic;
+  final List<BluetoothDevice> devicesList = [];
+
   final String _name;
 
   // Changer ces valeurs pour les vôtres.
-  final String _commandsSrvUUID = '15ff0fcd-6481-4565-9fe0-388628769cce';
-  final String _commandsCharUUID = '34a28b10-1486-4c61-9fa1-878296fd0262';
+  static const String BOT_SERVICE = '15ff0fcd-6481-4565-9fe0-388628769cce';
+  static const String BOT_CARACTERISTIQUE =
+      '34a28b10-1486-4c61-9fa1-878296fd0262';
 
-  
   /// Fonction de rappel, lorsque le robot est prêt à recevoir des commandes.
-  void Function()? onRobotReady; 
+  void Function()? onRobotReady;
 
-  bool get isConnected => _peripheral.isConnected && _peripheral.isReady;
+  bool get isConnected =>
+      connectedDevice != null && connectedDevice!.isConnected;
 
   /// Constructeur de la classe BLERobot
   BLERobot(this._name) {
-    _manager = BLEManager();
+    devicesList.clear();
+    connectedDevice = null;
+    commandCharacteristic = null;
+  }
 
-    // Fonction de rappel, lorsqu'un périphérique est trouvé
-    _manager.onPeripheralFound = (peripheral) async {
-      debugPrint('Périphérique trouvé: $peripheral, state: ${peripheral.state.value}');
-      _peripheral = peripheral;
-      _peripheral.onPeripheralReady = () async {
-        debugPrint('Robot connecté.');
-        onRobotReady?.call();
-      };
-      await _manager.stopDiscovery();
-      await _peripheral.connect();
+  /// Découvre les services disponibles sur le robot.
+  void startScan() {
+    devicesList.clear();
+    connectedDevice = null;
+    commandCharacteristic = null;
 
-      // On assigne le service et la caractéristique à utiliser.
-      await _peripheral.setActiveCharacteristic(_commandsSrvUUID, _commandsCharUUID);
+    FlutterBluePlus.startScan(timeout: const Duration(seconds: 5));
 
-      // On écoute les changements de valeur de la caractéristique.
-      _peripheral.characteristic.addListener(() {
-        debugPrint('Characteristic changed: ${_peripheral.characteristic.value}');
-      });  
-    };
+    FlutterBluePlus.scanResults.listen((results) {
+      for (ScanResult r in results) {
+        if (r.device.advName != '' && !devicesList.contains(r.device)) {
+          devicesList.add(r.device);
+
+          // Si le nom du périphérique correspond à celui recherché, on arrête la recherche et on se connecte.
+          if (r.device.advName == _name) {
+            FlutterBluePlus.stopScan();
+            _connectToDevice(r.device);
+          }
+        }
+      }
+    });
   }
 
   /// Démarre la découverte de périphériques BLE, afin de trouver le robot.
-  Future<void> tryConnect() async {
-    await _manager.startDiscovery(_name);
+  void _connectToDevice(BluetoothDevice device) async {
+    print("connexion à : ${device.advName} ( ${device.remoteId})");
+    await device.connect(timeout: const Duration(seconds: 90));
+    connectedDevice = device;
+    _discoverServices();
   }
 
+  void _discoverServices() async {
+    if (connectedDevice == null) return;
+
+    List<BluetoothService> services = await connectedDevice!.discoverServices();
+    for (var service in services) {
+      if (service.uuid.toString() == BOT_SERVICE) {
+        print("service trouvé: ${service.uuid}");
+        for (var characteristic in service.characteristics) {
+          if (characteristic.uuid.toString() == BOT_CARACTERISTIQUE) {
+            print("Characteristique trouvée: ${characteristic.uuid}");
+            commandCharacteristic = characteristic;
+            if (onRobotReady != null) {
+              onRobotReady!();
+            }
+            print("Robot connecté et prêt à recevoir des commandes.");
+            return;
+          }
+        }
+      }
+    }
+  }
 
   /// Envoie une commande au robot.
-  Future<void> sendCommand(RobotActionType action, String value) async {    
-    if(!isConnected) {
-      debugPrint('Périphérique non connecté.');
-      return;
-    }
-    if (value == "") {
-      value = "0";
-    }
-    String cmd = '${action.index.toString()}:$value';
-    await _peripheral.write(cmd);
-  }
+  Future<void> sendCommand(RobotActionType action, [String value = '']) async {
+    if (commandCharacteristic == null) return;
 
-  /// Envoie une demande de lecture de télémétrie au robot.
-  Future<String> readTelemetry(TelemetryType type) async {    
-    if(!isConnected) {
-      debugPrint('Périphérique non connecté.');
-      return '';
+    String fullCommand = action.name;
+    if (value != '') {
+      fullCommand += ':$value';
     }
-    return _peripheral.read(type.index.toString());
+    List<int> bytes = utf8.encode(fullCommand);
+    await commandCharacteristic!.write(bytes, withoutResponse: true);
   }
 }
--- lib/pages/functions_page_widget.dart
+++ lib/pages/functions_page_widget.dart
@@ -16,10 +16,10 @@ class _FunctionsWidgetState extends State<FunctionsWidget> {
   late BLERobot _robot;
   bool _connected = false;
 
- @override
+  @override
   void initState() {
     super.initState();
-    _robot = BLERobot('Rpi-NomDuRobot');
+    _robot = BLERobot('mlandry PI - bot');
     _robot.onRobotReady = () {
       // Appel de la fonction de rappel (réaffichage), lorsque le robot est prêt
       setState(() {
@@ -33,106 +33,106 @@ class _FunctionsWidgetState extends State<FunctionsWidget> {
     super.dispose();
   }
 
-  Future<void> startDiscovery() async {
-    await _robot.tryConnect();
+  void startDiscovery() {
+    _robot.startScan();
     debugPrint("Scan Bluetooth en cours...");
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: scaffoldKey,
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          automaticallyImplyLeading: false,
-          leading: Align(
-            alignment: const AlignmentDirectional(0.00, 0.00),
-            child: FaIcon(
-              FontAwesomeIcons.robot,
-              color: Theme.of(context).colorScheme.surface,
-              size: 32,
-            ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        automaticallyImplyLeading: false,
+        leading: Align(
+          alignment: const AlignmentDirectional(0.00, 0.00),
+          child: FaIcon(
+            FontAwesomeIcons.robot,
+            color: Theme.of(context).colorScheme.surface,
+            size: 32,
           ),
-          title: Text(
-            'Projet d\'intégration',
-            style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-              color: Theme.of(context).colorScheme.surface,
-            )
-          ),
-          actions: const [],
-          centerTitle: false,
-          elevation: 2,
         ),
-        body: SafeArea(
-          top: true,
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
+        title: Text('Projet d\'intégration',
+            style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                  color: Theme.of(context).colorScheme.surface,
+                )),
+        actions: const [],
+        centerTitle: false,
+        elevation: 2,
+      ),
+      body: SafeArea(
+        top: true,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
                   padding: const EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
                   child: TextButton(
                     style: ButtonStyle(
-                      backgroundColor: WidgetStateProperty.all<Color>(Colors.blueGrey),
-                      foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+                      backgroundColor:
+                          WidgetStateProperty.all<Color>(Colors.blueGrey),
+                      foregroundColor:
+                          WidgetStateProperty.all<Color>(Colors.white),
                     ),
-                    onPressed:  () async { await startDiscovery(); },
+                    onPressed: () {
+                      startDiscovery();
+                    },
                     child: const Text('Scan Bluetooth'),
-                  )
+                  )),
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all<Color>(Colors.red),
+                    foregroundColor:
+                        WidgetStateProperty.all<Color>(Colors.white),
+                  ),
+                  onPressed: () {
+                    // Vérification de la connexion avant de lancer la fonction
+                    if (_connected) {
+                      debugPrint("Fonction: Avancer 50cm");
+                      _robot.sendCommand(RobotActionType.Forward, '50');
+                    } else {
+                      debugPrint("Fonction: Avancer 50cm (non connecté)");
+                    }
+                  },
+                  child: const Text('Avancer 50cm'),
                 ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
-                  child: TextButton(
-                            style: ButtonStyle(
-                              backgroundColor: WidgetStateProperty.all<Color>(Colors.red),
-                              foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-                            ),
-                            onPressed: () { 
-                              // Vérification de la connexion avant de lancer la fonction
-                              if (_connected) {
-                                debugPrint("Fonction: Avancer 50cm");
-                                _robot.sendCommand(RobotActionType.Forward, '50');
-                              }
-                              else {
-                                debugPrint("Fonction: Avancer 50cm (non connecté)");
-                              }
-                            },
-                            child: const Text('Avancer 50cm'),
-                          ),
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 40),
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all<Color>(Colors.red),
+                    foregroundColor:
+                        WidgetStateProperty.all<Color>(Colors.white),
+                  ),
+                  onPressed: () async {
+                    // Vérification de la connexion avant de lancer la fonction
+                    if (_connected) {
+                      debugPrint("Fonction: Arrêt d'urgence");
+                      _robot.sendCommand(RobotActionType.Stop, '0');
+                    } else {
+                      debugPrint("Fonction: Arrêt d'urgence (non connecté)");
+                    }
+                  },
+                  child: const Text('Arrêt d\'urgence'),
                 ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 40),
-                  child: TextButton(
-                            style: ButtonStyle(
-                              backgroundColor: WidgetStateProperty.all<Color>(Colors.red),
-                              foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
-                            ),
-                            onPressed: () async { 
-                              // Vérification de la connexion avant de lancer la fonction
-                              if(_connected) {
-                                debugPrint("Fonction: Arrêt d'urgence");
-                                _robot.sendCommand(RobotActionType.Stop, '0');
-                              }
-                              else {
-                                debugPrint("Fonction: Arrêt d'urgence (non connecté)");
-                              }
-                            },
-                            child: const Text('Arrêt d\'urgence'),
-                          ),
-                ),
-                const Divider(
-                  thickness: 2,
-                  indent: 60,
-                  endIndent: 60,
-                  color: Color(0xCC3A3A3A),
-                ),                
-              ],
-            ),
+              ),
+              const Divider(
+                thickness: 2,
+                indent: 60,
+                endIndent: 60,
+                color: Color(0xCC3A3A3A),
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 }
